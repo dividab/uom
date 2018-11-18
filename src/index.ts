5732014f6b3ee99f538596d0545924ab6eaecf3a
Jonas Kello <jonas.kello@gmail.com>
2018-11-18 15:08:50 +0100

    Remove quantity info

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as Amount from "./amount";
 import * as Quantity from "./quantity";
-import * as QuantityInfo from "./quantity-info";
 import * as Unit from "./unit";
 import * as Units from "./units";
 import * as UnitDivide from "./unit-divide";
@@ -10,7 +9,6 @@ import * as Format from "./format";
 export {
   Amount,
   Quantity,
-  QuantityInfo,
   Unit,
   Units,
   UnitDivide,
