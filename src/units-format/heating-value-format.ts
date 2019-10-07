62cf2971cc9d066977607a1b178e8493cb35dc36
Johan <johan1kristiansson@gmail.com>
2019-10-07 14:33:13 +0200

    Added Units KiloJoulePerCubicMeter & BtuPerCubicFeet

--- src/units-format/heating-value-format.ts
+++ src/units-format/heating-value-format.ts
@@ -4,3 +4,7 @@ import { createUnitFormat } from "../unit-format";
 
 // Energy per volume
 export const KiloWattHourPerCubicMeter = createUnitFormat("kWh/m³", 2);
+
+export const KiloJoulePerCubicMeter = createUnitFormat("kJ/m³", 2);
+
+export const BtuPerCubicFeet = createUnitFormat("BTU/ft³", 2);
