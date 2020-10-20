db7f5caae7d885ed08ef57833c3b0a0630905055
Jonas Kello <jonas.kello@gmail.com>
2020-10-20 16:10:39 +0200

    Cleanup config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,3 @@
-// module.exports = {
-//   preset: "ts-jest",
-//   testEnvironment: "node",
-//   testRegex: "./test/.+\\.test\\.ts$",
-//   collectCoverage: true,
-//   collectCoverageFrom: ["src/**/*.ts"],
-//   moduleFileExtensions: ["ts", "js", "json", "node"],
-//   coverageReporters: ["text-summary", "lcov"],
-//   globals: {
-//     "ts-jest": {
-//       tsConfig: "<rootDir>/test/tsconfig.json"
-//     }
-//   }
-// };
-
 module.exports = {
   preset: "ts-jest",
   testMatch: ["<rootDir>/src/**/*.test.{ts,tsx}"],
